60786ecdc3d52c6d96346438e25cacfb861da552
tviana <tviana@G029378>
2019-07-10 14:21:22 +0100

    lesson 4

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -9,6 +9,7 @@ namespace ConsoleApp1
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello Thiago!");
             Console.WriteLine("Hello World 2!");
+            Console.WriteLine("Hello World!");
         }
     }
 }
